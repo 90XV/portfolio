2a1f1c6a28c443f2be1d8569837cdc854f0aea0b
90XV <g2201173@gmail.com>
2025-04-18 12:58:49 +0800

    added pages

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index'))
 
 #import routes
 @app.route('/')
@@ -9,15 +13,23 @@ def index():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/contact', methods=['GET'])
 def contact():
     return render_template("contact.html")
 
-@app.route('/profile', methods=['GET'])
-def profile():
-    return render_template("profile.html")
+@app.route('/skills', methods=['GET'])
+def skills():
+    return render_template("skills.html")
+
+@app.route('/fyp', methods=['GET'])
+def fyp():
+    return render_template("fyp.html") 
+
+@app.route('/personal', methods=['GET'])
+def personal():
+    return render_template("personal.html") 
 
 if __name__ == '__main__':
     app.run()
